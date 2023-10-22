a5e71bfa6d40048979459d0e94c728a1d5ad115f
Matias Chanquia <matiaschanquianahuel@gmail.com>
2023-10-22 00:26:28 -0300

    footer hecho con boton de like

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css"; 
 import AboutMe from "./components/AboutMe";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Proyects from "./components/Proyects";
 
@@ -9,6 +10,7 @@ function App() {
 			<Header />
 			<AboutMe />
 			<Proyects />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { getCurrentYear } from "../utils/day";
+import "./Footer.css";
+import { useEffect, useRef } from "react";
+
+const Footer = () => {
+
+	const likeRef = useRef(null);
+
+	useEffect(() => {
+		const likeLS = localStorage.getItem("like");
+
+		if(likeLS) {
+			likeRef.current.classList.remove("active");
+		}
+	}, []);
+
+	const handleClick = (e) => {
+		const likeLS = localStorage.getItem("like");
+		if(likeLS) {
+			console.log("No puedes volver a dar like");
+			return;
+		}
+
+		let tag = e.target;
+		if(!e.target.classList.contains("like-btn")) {
+			tag = tag.parentElement;
+		}
+		tag.classList.remove("active");
+
+		axios.post("https://portfolio-backend-chanquia.up.railway.app/api/likes")
+			.then(() => {
+				localStorage.setItem("like", JSON.stringify(true));
+				console.log("Like dado correctamente");
+			})
+			.catch(err => {
+				console.log(err);
+				tag.classList.add("active");
+			});
+	};
+
+	return(
+		<footer className="footer">
+			<div className="mensaje-like">
+				Deja tu me gusta <ion-icon name="arrow-forward-outline"></ion-icon> 
+				<div className="like-btn active" onClick={handleClick} ref={likeRef}>
+					<ion-icon name="heart-outline"></ion-icon>
+				</div>
+			</div>
+			<p>
+				&copy; Copyright Matias Chanquia - {getCurrentYear()}
+			</p>
+		</footer>
+	);
+};
+
+export default Footer;
